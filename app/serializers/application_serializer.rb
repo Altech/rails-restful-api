82ba982c7bd73679f07b88244def548b0306ca03
Altech <takeno.sh@gmail.com>
2017-04-20 11:45:35 +0900

    Add .preload

--- app/serializers/application_serializer.rb
+++ app/serializers/application_serializer.rb
@@ -1,2 +1,10 @@
 class ApplicationSerializer < ActiveModel::Serializer
+  class << self
+    def preload(&block)
+      @preload_rule = Api::PreloadRule.new
+      @preload_rule.instance_exec(&block)
+      @preload_rule
+    end
+    attr_reader :preload_rule
+  end
 end
